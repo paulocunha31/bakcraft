3f4c3f6fb0e697727040029561e3b291008ba179
paulocunha31 <pauloroberto_odc@hotmail.com>
2025-08-23 17:44:40 -0300

    feat: atualizacao em script.js

--- js/script.js
+++ js/script.js
@@ -2,30 +2,76 @@
 // Ativar Links do Menu
 const links = document.querySelectorAll(".header-menu a")
 
-function menuAtivo(link){
-const url = location.href
-const href = link.href
+function menuAtivo(link) {
+    const url = location.href
+    const href = link.href
 
-if(url.includes(href)){
-   link.classList.add("ativo") 
-}
+    if (url.includes(href)) {
+        link.classList.add("ativo")
+    }
 
 }
 
 links.forEach(menuAtivo)
 
+
 // Ativar Itens do Orcamento
 
 const paramentos = new URLSearchParams(location.search)
 
-function ativaProduto(paramento){
+function ativaProduto(paramento) {
     const elemento = document.getElementById(paramento)
 
-    if(elemento) {
+    if (elemento) {
         elemento.checked = true
 
     }
+}
+paramentos.forEach(ativaProduto)
+
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button")
+
+function ativarPergunta(event) {
+    const pergunta = event.target
+    const controls = pergunta.getAttribute("aria-controls")
+    const resposta = document.getElementById(controls)
+
+    resposta.classList.toggle("ativa")
+    const ativa = resposta.classList.contains("ativa")
+    pergunta.setAttribute("aria-expanded", ativa)
 
 }
 
-paramentos.forEach(ativaProduto)
+function eventosPeguntas(pergunta) {
+    pergunta.addEventListener("click", ativarPergunta)
+}
+
+perguntas.forEach(eventosPeguntas)
+
+
+// Galeria de Bicicletas
+const galeriaContainer = document.querySelector(".bicicleta-imagem")
+const galeria = document.querySelectorAll(".bicicleta-imagem img")
+
+function trocarImagem (event) {
+    const img = event.target
+    const media = matchMedia("(min-width: 935px)").matches
+
+    if(media) {
+        galeriaContainer.prepend(img)
+    }
+}
+
+function eventosGaleria(img){
+    img.addEventListener("click", trocarImagem)
+}
+
+galeria.forEach(eventosGaleria)
+
+// Animação
+if(window.SimpleAnime){
+    new SimpleAnime()
+}
